04a94f212cada3e61b88e303cb4ef58686f84a95
Jason Ward <jason.ward@policystat.com>
2014-09-19 15:16:22 -0400

    refs #2: Got the error state.

--- docx_to_html/tests.py
+++ docx_to_html/tests.py
@@ -1,3 +1,5 @@
+import json
+
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
@@ -11,6 +13,18 @@ class CodeCoverageTestCase(TestCase):
 class IndexTestCase(TestCase):
     url = reverse('index')
 
-    def test_GET_returns_200(self):
+    def post(self, params=None, status_code=302):
+        if params is None:
+            params = {}
+        r = self.client.post(self.url, params)
+        self.assertEqual(r['Content-Type'], 'application/json')
+        self.assertEqual(r.status_code, status_code)
+        return json.loads(r.content)
+
+    def test_GET_returns_405(self):
         r = self.client.get(self.url)
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.status_code, 405)
+
+    def test_empty_POST_returns_JSON_response_with_errors(self):
+        r = self.post(status_code=200)
+        self.assertEqual(r, {u'file': [u'This field is required.']})
--- docx_to_html/views.py
+++ docx_to_html/views.py
@@ -1,5 +1,26 @@
+import json
+
 from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+
+from docx_to_html.forms import UploadFileForm
 
 
+class JSONResponse(HttpResponse):
+    def __init__(self, context, status=302, **kwargs):
+        kwargs.update({
+            'content_type': 'application/json',
+            'status': status,
+        })
+        return super(JSONResponse, self).__init__(
+            json.dumps(context),
+            **kwargs
+        )
+
+
+@require_POST
 def index(request):
-    return HttpResponse('Test')
+    form = UploadFileForm(request.POST, request.FILES)
+    if form.is_valid():
+        return JSONResponse({})
+    return JSONResponse(form.errors, status=200)
